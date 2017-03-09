458f5645a2c5077bc1ef3605fb5ddafef818174b
Curtis Loisel <curtis@voceconnect.com>
2017-03-09 11:34:16 -0500

    Storing seen posts and excluding from query

--- csloisel-random-post.php
+++ csloisel-random-post.php
@@ -5,9 +5,12 @@
 
 class CSLoisel_Random_Post {
 
+	const COOKIE_KEY = 'CSLSeenRandomPosts';
+
 	public static function init() {
 		if ( self::is_random_post_page() ) {
 			add_action( 'pre_get_posts', array( __CLASS__, 'query_random_post' ) );
+			add_action( 'wp', array( __CLASS__, 'store_seen_post' ) );
 		}
 	}
 
@@ -25,7 +28,25 @@ class CSLoisel_Random_Post {
 			$query->set( 'orderby', 'rand' );
 			$query->set( 'post_type', 'post' );
 			$query->set( 'posts_per_page', 1 );
+			$query->set( 'exclude', self::get_seen_posts() );
 		}
+	}
+
+	public static function get_seen_posts() {
+		$seen_ids = !empty( $_COOKIE[ self::COOKIE_KEY ] ) ? json_decode( $_COOKIE[ self::COOKIE_KEY ] ) : array();
+		$seen_ids = array_map( 'intval' , $seen_ids );
+		$seen_ids = array_filter( $seen_ids );
+
+		return $seen_ids;
+	}
+
+	public static function store_seen_post( $wp ) {
+		$post_id = get_queried_object_id();
+		$seen_ids = self::get_seen_posts();
+
+		$seen_ids[] = $post_id;
+
+		setcookie( self::COOKIE_KEY, json_encode( $seen_ids ), time() + MONTH_IN_SECONDS );
 	}
 
 }
